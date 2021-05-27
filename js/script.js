7c45f9a745d02e82c8acdae68d7cc875b68701c1
vvalcarcell <valcarcellvalentina@gmail.com>
2021-05-27 16:17:30 +0200

    pass-generator

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var fname = prompt('Hi! What is your name?');
+
+var lname = prompt('And surname?');
+
+var color = prompt('What is your favorite color?');
+
+document.getElementById('pass').innerHTML = "Your password is: " + fname + lname + color + "21";
